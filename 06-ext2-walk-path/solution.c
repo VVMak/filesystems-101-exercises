8539b32e7ea20066ccc9394b6755844387d9dc3f
Vladimir Makarov <makarov.vladimir@phystech.edu>
2022-11-11 01:44:40 +0300

    try another solution

--- 06-ext2-walk-path/solution.c
+++ 06-ext2-walk-path/solution.c
@@ -46,126 +46,6 @@ int get_inode(struct ext2_inode* inode, int img, struct ext2_super_block* sb, si
 	return (res < 0 ? -errno : res);
 }
 
-bool get_next_filename(const char* path, char* filename) {
-	if (path[0] != '/') {
-		return false;
-	}
-	path += 1;
-	memset(filename, '\0', EXT2_NAME_LEN + 1);
-	const char* pos = strchr(path, '/');
-	if (pos == NULL) {
-		pos = path + strlen(path);
-	}
-	strncpy(filename, path, pos - path);
-	return true;
-}
-
-int handle_dir_block(int img, size_t block_nr, long block_size, const char* file, int* remained_bytes) {
-	assert(*remained_bytes > 0);
-	char* block = malloc(block_size);
-	if (pread(img, block, block_size, block_nr * block_size) < 0) {
-		free(block);
-		return -errno;
-	}
-	struct ext2_dir_entry_2* dir_entry = (struct ext2_dir_entry_2*)block;
-	while ((char*)dir_entry - block < block_size && dir_entry->inode > 0) {
-		if (strlen(file) == dir_entry->name_len && !strncmp(file, dir_entry->name, dir_entry->name_len)) {
-			int inode_nr = dir_entry->inode;
-			free(block);
-			return inode_nr;
-		}
-		dir_entry = (struct ext2_dir_entry_2*)((char*)dir_entry + dir_entry->rec_len);
-	}
-	*remained_bytes -= block_size;
-	free(block);
-	return -ENOENT;
-}
-
-int handle_ind_block(int img, size_t block_nr, long block_size, const char* file, int* remained_bytes) {
-	if (*remained_bytes <= 0) {
-		return -ENOENT;
-	}
-	uint32_t* redir = malloc(block_size);
-	int res = pread(img, redir, block_size, block_size * block_nr);
-	if (res < 0) {
-		free(redir);
-		return -errno;
-	}
-	size_t MAX_REDIRECT_BLOCKS = block_size / sizeof(uint32_t);
-	for (size_t i = 0; i < MAX_REDIRECT_BLOCKS && *remained_bytes > 0; ++i) {
-		if ((res = handle_dir_block(img, redir[i], block_size, file, remained_bytes)) != -ENOENT) {
-			free(redir);
-			return res;
-		}
-	}
-	free(redir);
-	return -ENOENT;
-}
-
-int handle_d_ind_block(int img, size_t block_nr, long block_size, const char* file, int* remained_bytes) {
-	if (*remained_bytes <= 0) {
-		return -ENOENT;
-	}
-	uint32_t* redir = malloc(block_size);
-	int res = pread(img, redir, block_size, block_size * block_nr);
-	if (res < 0) {
-		free(redir);
-		return -errno;
-	}
-	size_t MAX_REDIRECT_BLOCKS = block_size / sizeof(uint32_t);
-	for (size_t i = 0; i < MAX_REDIRECT_BLOCKS && *remained_bytes > 0; ++i) {
-		if ((res = handle_ind_block(img, redir[i], block_size, file, remained_bytes)) != -ENOENT) {
-			free(redir);
-			return res;
-		}
-	}
-	free(redir);
-	return -ENOENT;
-}
-
-int find_inode(int img, struct ext2_super_block* sb, int inode_nr, const char* path) {
-	int res;
-	struct ext2_inode inode;
-	int block_size = EXT2_BLOCK_SIZE(sb);
-	char filename[EXT2_NAME_LEN + 1];
-	while (get_next_filename(path, filename)) {
-		path += strlen(filename) + 1;
-		if ((res = get_inode(&inode, img, sb, inode_nr)) < 0) {
-			return res;
-		}
-		if (!S_ISDIR(inode.i_mode)) {
-			return -ENOTDIR;
-		}
-		int remained_bytes = inode.i_size;
-		bool found = false;
-		for (size_t i = 0; i < EXT2_NDIR_BLOCKS && remained_bytes > 0; ++i) {
-			if ((res = handle_dir_block(img, inode.i_block[i], block_size, filename, &remained_bytes)) >= 0) {
-				inode_nr = res;
-				found = true;
-				break;
-			} else if (res != -ENOENT) {
-				return res;
-			}
-		}
-		if (found) { continue; }
-		if ((res = handle_ind_block(img, inode.i_block[EXT2_IND_BLOCK], block_size, filename, &remained_bytes)) >= 0) {
-			inode_nr = res;
-			continue;
-		} else if (res != -ENOENT) {
-			return res;
-		}
-		if ((res = handle_d_ind_block(img, inode.i_block[EXT2_DIND_BLOCK], block_size, filename, &remained_bytes)) >= 0) {
-			inode_nr = res;
-			continue;
-		} else if (res != -ENOENT) {
-			return res;
-		}
-		
-		return -ENOENT;
-	}
-	return inode_nr;
-}
-
 int write_data_block(uint32_t block_nr, uint32_t block_size, int img, int out, size_t file_size) {
 	int size = min(file_size, block_size);
 	void* block = malloc(size);
@@ -244,6 +124,125 @@ int copy_file(int img, struct ext2_super_block* sb, int inode_nr, int out) {
 	return 0;
 }
 
+bool get_next_filename(const char* path, char* filename) {
+	if (path[0] != '/') {
+		return false;
+	}
+	path += 1;
+	memset(filename, '\0', EXT2_NAME_LEN + 1);
+	const char* pos = strchr(path, '/');
+	if (pos == NULL) {
+		pos = path + strlen(path);
+	}
+	strncpy(filename, path, pos - path);
+	return true;
+}
+
+int find_inode(int img, int out, struct ext2_super_block* sb, int inode_nr, const char* path);
+
+int handle_dir_block(int img, int out, size_t block_nr, struct ext2_super_block* sb, const char* path) {
+  const int block_size = EXT2_BLOCK_SIZE(sb);
+	char* block = malloc(block_size);
+	if (pread(img, block, block_size, block_nr * block_size) < 0) {
+		free(block);
+		return -errno;
+	}
+	struct ext2_dir_entry_2* dir_entry = (struct ext2_dir_entry_2*)block;
+	path += 1;
+	while ((char*)dir_entry - block < block_size) {
+		if (dir_entry->inode == 0) {
+			break;
+		}
+		const char* pos = strchr(path, '/');
+		int len = (pos == NULL ? (int)strlen(path) : pos - path);
+    if (dir_entry->name_len == len && !strncmp(path, dir_entry->name, dir_entry->name_len)) {
+			// printf("%d: %s\n", dir_entry->name_len, dir_entry->name);
+      int inode_nr = dir_entry->inode;
+      if (pos == NULL) {
+        free(block);
+        return copy_file(img, sb, inode_nr, out);
+      }
+      if (dir_entry->file_type != EXT2_FT_DIR) {
+        free(block);
+        return -ENOTDIR;
+      }
+      free(block);
+      return find_inode(img, out, sb, inode_nr, pos);
+    }
+		dir_entry = (struct ext2_dir_entry_2*)((char*)dir_entry + dir_entry->rec_len);
+	}
+	free(block);
+	return -ENOENT;
+}
+
+int find_inode(int img, int out, struct ext2_super_block* sb, int inode_nr, const char* path) {
+  struct ext2_inode inode;
+  int res;
+	if ((res = get_inode(&inode, img, sb, inode_nr)) < 0) {
+		return res;
+	}
+	const int block_size = EXT2_BLOCK_SIZE(sb);
+	int remained_bytes = inode.i_size;
+	for (size_t i = 0; i < EXT2_NDIR_BLOCKS && remained_bytes > 0; ++i) {
+		int res = handle_dir_block(img, out, inode.i_block[i], sb, path);
+    if (res != -ENOENT) {
+      return res;
+    }
+		remained_bytes -= block_size;
+	}
+	if (remained_bytes <= 0) {
+		return -ENOENT;
+	}
+	
+	// ------------------
+
+	uint32_t* redir_1 = malloc(block_size);
+	res = pread(img, redir_1, block_size, block_size * inode.i_block[EXT2_IND_BLOCK]);
+	if (res < 0) {
+		return -errno;
+	}
+	size_t MAX_REDIRECT_BLOCKS = block_size / sizeof(uint32_t);
+	for (size_t i = 0; i < MAX_REDIRECT_BLOCKS && remained_bytes > 0; ++i) {
+		int handle_res = handle_dir_block(img, out, redir_1[i], sb, path);
+		if (handle_res != -ENOENT) {
+			free(redir_1);
+			return handle_res;
+		}
+		remained_bytes -= block_size;
+	}
+	if (remained_bytes <= 0) {
+		free(redir_1);
+		return -ENOENT;
+	}
+
+
+	res = pread(img, redir_1, block_size, inode.i_block[EXT2_DIND_BLOCK] * block_size);
+	if (res < 0) {
+		return -errno;
+	}
+	uint32_t* redir_2 = malloc(block_size);
+	for (size_t i = 0; i < MAX_REDIRECT_BLOCKS && remained_bytes > 0; ++i) {
+		res = pread(img, redir_2, block_size, redir_1[i] * block_size);
+		if (res < 0) {
+			free(redir_1);
+			free(redir_2);
+			return -errno;
+		}
+		for (size_t j = 0; j < MAX_REDIRECT_BLOCKS && remained_bytes > 0; ++j) {
+			int handle_res = handle_dir_block(img, out, redir_2[j], sb, path);
+			if (handle_res != -ENOENT) {
+				free(redir_1);
+				free(redir_2);
+				return handle_res;
+			}
+			remained_bytes -= block_size;
+		}
+	}
+	free(redir_1);
+	free(redir_2);
+	return -ENOENT;
+}
+
 int dump_file(int img, const char *path, int out)
 {
 	int res;
@@ -251,12 +250,8 @@ int dump_file(int img, const char *path, int out)
 	if ((res = read_sb(&sb, img)) < 0) {
 		return res;
 	}
-	if ((res = find_inode(img, &sb, 2, path)) < 0) {
-		assert(res != -ENOTDIR);
+	if ((res = find_inode(img, out, &sb, 2, path)) < 0) {
 		return res;
-	}
-	if ((res = copy_file(img, &sb, res, out)) < 0) {
-	  return res;
 	}
 	return 0;
 }
